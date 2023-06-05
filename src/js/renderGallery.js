c3ddd9f6c7f90947f81cdd1e7467278e56418ca5
ApolinarySzumski <apolinary@poczta.onet.pl>
2023-06-05 16:56:14 +0200

    finished task

---
+++ src/js/renderGallery.js
@@ -0,0 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+let lightbox = new SimpleLightbox('.photo-link', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+const renderGallery = photoArray => {
+  const galleryItem = photoArray
+    .map(img => {
+      return `<a class="photo-link" href="${img.largeImageURL}">
+                                <div class="photo-card">
+                                    <img class="photo-img" src="${img.webformatURL}" data-source="${img.largeImageURL}" alt="${img.tags}" width=100% loading="lazy" />
+                                    <div class="info">
+                                        <p class="info-item">
+                                            <b>Likes</b>
+                                            ${img.likes}
+                                        </p>
+                                        <p class="info-item">
+                                            <b>Views</b>
+                                            ${img.views}
+                                        </p>
+                                        <p class="info-item">
+                                            <b>Comments</b>
+                                            ${img.comments}
+                                        </p>
+                                        <p class="info-item">
+                                            <b>Downloads</b>
+                                            ${img.downloads}
+                                        </p>
+                                    </div>
+                                </div>
+                            </a>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', galleryItem);
+  lightbox.refresh();
+};
+
+export default renderGallery;
